126d6f900152c32ad532c87c153dec12fe11c32b
AB-IN-lsy <liusy200708@163.com>
2024-06-22 20:01:01 +0800

    2024.6.22

--- templatecf.py
+++ templatecf.py
@@ -1,39 +1,81 @@
 # import
 from sys import setrecursionlimit, stdin, stdout, exit
-from collections import Counter, deque
+from collections import Counter, deque, defaultdict
 from heapq import heapify, heappop, heappush, nlargest, nsmallest
 from bisect import bisect_left, bisect_right
 from datetime import datetime, timedelta
 from string import ascii_lowercase, ascii_uppercase
 from math import log, gcd, sqrt, fabs, ceil, floor
+from types import GeneratorType
+from typing import TypeVar, List, Dict, Any, Callable
 
 
-class sa:
+# Data structure
+class SA:
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def __lt__(self, a):
-        return self.x < a.x
+    def __lt__(self, other):
+        return self.x < other.x
 
 
-# Final
-N = int(1e5 + 10)
+# Constants
+N = int(2e5 + 10)  # If using AR, modify accordingly
 M = 20
 INF = int(2e9)
 
-# Define
+# Set recursion limit
 setrecursionlimit(INF)
+
+# Read
 input = lambda: stdin.readline().rstrip("\r\n")  # Remove when Mutiple data
 read = lambda: map(int, input().split())
-AR = lambda x=0: [x] * N
+read_list = lambda: list(map(int, input().split()))
+
+
+# Func
+class std:
+
+    # Recursion
+    @staticmethod
+    def bootstrap(f, stack=None):
+        if stack is None:
+            stack = []
+
+        def wrappedfunc(*args, **kwargs):
+            if stack:
+                return f(*args, **kwargs)
+            else:
+                to = f(*args, **kwargs)
+                while True:
+                    if isinstance(to, GeneratorType):
+                        stack.append(to)
+                        to = next(to)
+                    else:
+                        stack.pop()
+                        if not stack:
+                            break
+                        to = stack[-1].send(to)
+                return to
+
+        return wrappedfunc
+
+    letter_to_num = staticmethod(lambda x: ord(x.upper()) - 65)  # A -> 0
+    num_to_letter = staticmethod(lambda x: ascii_uppercase[x])  # 0 -> A
+    array = staticmethod(lambda x=0, size=N: [x] * size)
+    array2d = staticmethod(
+        lambda x=0, rows=N, cols=M: [std.array(x, cols) for _ in range(rows)])
+    max = staticmethod(lambda a, b: a if a > b else b)
+    min = staticmethod(lambda a, b: a if a < b else b)
+    filter = staticmethod(lambda func, iterable: list(filter(func, iterable)))
+
 
 # —————————————————————Division line ——————————————————————
 
 
 def solve():
-    n, = read()
-    a = read()
 
     return
 
