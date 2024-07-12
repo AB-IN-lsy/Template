542dc42686df8e0928460f8bcfe2b0476ea12242
AB-IN-lsy <liusy200708@163.com>
2024-07-12 19:05:46 +0800

    2024.7.12

---
+++ .vscode/shortcuts.md
@@ -0,0 +1,9 @@
+* `ctrl + alt + n` ：运行程序
+* `ctrl + alt + l` ：格式化
+* `ctrl + alt + o` ：整理格式化语句
+* `ctrl + alt + n` ：运行程序
+* `ctrl + alt + i` ：插入头注释
+* `ctrl + ~ `：打开终端
+* `ctrl + 1` ：运行自定义任务
+* `ctrl + alt + p` ：显示所以命令
+* `ctrl + i` ：触发提示
--- templatelc.py
+++ templatelc.py
@@ -1,78 +1,42 @@
-# import
-from sys import setrecursionlimit, stdin, stdout, exit
-from collections import Counter, deque, defaultdict
-from heapq import heapify, heappop, heappush, nlargest, nsmallest
-from bisect import bisect_left, bisect_right
+# 3.8.19 import
+import random
+from collections import Counter, defaultdict, deque
 from datetime import datetime, timedelta
+from functools import lru_cache
+from heapq import heapify, heappop, heappush, nlargest, nsmallest
+from itertools import combinations, compress, permutations, starmap, tee
+from math import ceil, comb, fabs, floor, gcd, log, perm, sqrt
 from string import ascii_lowercase, ascii_uppercase
-from math import log, gcd, sqrt, fabs, ceil, floor
-from types import GeneratorType
-from typing import TypeVar, List, Dict, Any, Callable
-
-
-# Data structure
-class SA:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __lt__(self, other):
-        return self.x < other.x
-
+from sys import exit, setrecursionlimit, stdin
+from typing import Any, Dict, List, Tuple, TypeVar, Union
 
 # Constants
-N = int(2e5 + 10)  # If using AR, modify accordingly
-M = 20
-INF = int(2e9)
+TYPE = TypeVar('TYPE')
+N = int(2e5 + 10)
+M = int(20)
+INF = int(1e12)
+OFFSET = int(100)
+MOD = int(1e9 + 7)
 
 # Set recursion limit
-setrecursionlimit(INF)
-
-# Read
-input = lambda: stdin.readline().rstrip("\r\n")  # Remove when Mutiple data
-read = lambda: map(int, input().split())
-read_list = lambda: list(map(int, input().split()))
+setrecursionlimit(int(2e9))
 
 
-# Func
-class std:
+class Arr:
+    array = staticmethod(lambda x=0, size=N: [x() if callable(x) else x for _ in range(size)])
+    array2d = staticmethod(lambda x=0, rows=N, cols=M: [Arr.array(x, cols) for _ in range(rows)])
+    graph = staticmethod(lambda size=N: [[] for _ in range(size)])
 
-    # Recursion
-    @staticmethod
-    def bootstrap(f, stack=None):
-        if stack is None:
-            stack = []
 
-        def wrappedfunc(*args, **kwargs):
-            if stack:
-                return f(*args, **kwargs)
-            else:
-                to = f(*args, **kwargs)
-                while True:
-                    if isinstance(to, GeneratorType):
-                        stack.append(to)
-                        to = next(to)
-                    else:
-                        stack.pop()
-                        if not stack:
-                            break
-                        to = stack[-1].send(to)
-                return to
-
-        return wrappedfunc
-
-    letter_to_num = staticmethod(lambda x: ord(x.upper()) - 65)  # A -> 0
-    num_to_letter = staticmethod(lambda x: ascii_uppercase[x])  # 0 -> A
-    array = staticmethod(lambda x=0, size=N: [x] * size)
-    array2d = staticmethod(
-        lambda x=0, rows=N, cols=M: [std.array(x, cols) for _ in range(rows)])
+class Math:
     max = staticmethod(lambda a, b: a if a > b else b)
     min = staticmethod(lambda a, b: a if a < b else b)
-    filter = staticmethod(lambda func, iterable: list(filter(func, iterable)))
 
 
-# —————————————————————Division line ——————————————————————
+class Std:
+    pass
+
+# ————————————————————— Division line ——————————————————————
 
 
 class Solution:
